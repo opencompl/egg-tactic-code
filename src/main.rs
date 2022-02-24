a19bb796097a367ee4d42ef5631270ca2bf1797b
Andres Goens <andres.goens@ed.ac.uk>
2022-02-24 17:27:39 +0000

    add explanations + more examples from the Knuth-Bendix completion for groups

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,39 @@ fn main() {
       rw!("inv-left";  "(* (^-1 ?a) ?a)" => "1"),
       rw!("inv-left'";  "1" => "(* (^-1 a) a)"),
       rw!("mul-one";  "(* ?a 1)" => "?a"),
+      rw!("mul-one'";  "?a" => "(* ?a 1)" ),
       rw!("inv-right";  "(* ?a (^-1 ?a))" => "1"),
       rw!("inv-right'";  "1" => "(* a (^-1 a))"),
+      //rw!("inv-right''";  "1" => "(* b (^-1 b))"),
 
   ];
 
     // We can make our own Language later to work with other types.
     //(a * b) * (1⁻¹⁻¹ * b⁻¹ * ((a⁻¹ * a⁻¹⁻¹⁻¹) * a))
-    let start = "(* (* a b)(* (^-1 (^-1 1))  (* (^-1 b) (* (* (^-1 a) (^-1 (^-1 (^-1 a)))) a))))".parse().unwrap();
+    //let start = "(* (* a b)(* (^-1 (^-1 1))  (* (^-1 b) (* (* (^-1 a) (^-1 (^-1 (^-1 a)))) a))))".parse().unwrap();
 
-    //let start = "(^-1 (^-1 a))".parse().unwrap();
+    // a⁻¹⁻¹ = a
+    let start = "(^-1 (^-1 a))".parse().unwrap();
+
+    //  a⁻¹ * (a * b) = b
+    //let start = "(* (^-1  a) (* a b))".parse().unwrap();
+
+    //  a * (a⁻¹ * b) = b
+    //let start = "(* a (* (^-1  a) b))".parse().unwrap();
+
+    //(a * b)⁻¹ = b⁻¹ * a⁻¹
+    //let start = "(^-1 (* a b))".parse().unwrap();
+    //let start = "(* (^-1 b) (^-1 a))".parse().unwrap();
+    // it won't get this one!
+
+    //(1 : G)⁻¹ = 1
+    //let start = "(^-1 1)".parse().unwrap();
+
     // That's it! We can run equality saturation now.
-    let mut runner = Runner::default().with_expr(&start)
-        //.with_node_limit(15)
+    let mut runner = Runner::default()
+        //.with_node_limit(800)
+        .with_explanations_enabled()
+        .with_expr(&start)
         .run(rules);
 
     // Dump
@@ -41,6 +61,8 @@ fn main() {
     // Luckily the runner stores the eclass Id where we put the initial expression.
     let (best_cost, best_expr) = extractor.find_best(runner.roots[0]);
 
-    // we found the best thing, which is just "a" in this case
+
     println!("Best expr : {:?} -- cost: {}",best_expr, best_cost);
+    println!("{}", runner.explain_equivalence(&start, &best_expr).get_flat_string());
+    // we found the best thing, which is just "a" in this case
 }
