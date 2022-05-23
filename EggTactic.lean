a89f219ea4af01c5b671eb644ca933197ea04322
Siddharth Bhat <siddu.druid@gmail.com>
2022-05-23 23:24:54 +0100

    Extract out equalities (term : a = b) from env
    This is necessary for us to find rewrites to pass to lean.

--- EggTactic.lean
+++ EggTactic.lean
@@ -8,27 +8,43 @@ open Lean Meta Elab Tactic
 open Lean.Elab.Term
 
 
+-- Path to the egg server.
+def eggServerPath : String := "/home/bollu/work/egg/egg-tactic-code/json-egg/target/debug/egg-herbie"
+
+
 elab "myTactic" : tactic =>  do
   let goals <- getGoals
   let target <- getMainTarget
   match target.eq? with
   | none =>  throwError "target {target} is not an equality"
-  | some (equalityType, equalityLhs, equalityRhs) =>
+  | some (equalityTermType, equalityLhs, equalityRhs) =>
     let maingoal <- getMainGoal
     liftMetaTactic fun mvarId => do
       -- let (h, mvarId) <- intro1P mvarId
       -- let goals <- apply mvarId (mkApp (mkConst ``Or.elim) (mkFVar h))
       let lctx <- getLCtx
       let mctx <- getMCtx
-      let hypsOfType <- lctx.foldlM (init := []) (fun accum decl =>  do
-          if decl.type == equalityType
+      let hypsOfEqualityTermType <- lctx.foldlM (init := []) (fun accum decl =>  do
+          if decl.type == equalityTermType
           then return (decl.userName, decl.type) :: accum
           else return accum)
+
+      let hypsOfEquality <- lctx.foldlM (init := []) (fun accum decl =>  do
+          match decl.type.eq? with
+          | none => return accum
+          | some (eqType', _, _) => do
+            let isEqOfCorrectType <- isExprDefEq equalityTermType eqType'
+             -- TODO: extract only if eqType = eqType'.
+             -- Yes I see the irony.
+             if isEqOfCorrectType
+             then return (decl.userName, decl.value) :: accum
+             else return accum)
       let out := "\n====\n"
-      let out := out ++ m!"-equalityType: {equalityType}\n"
-      let out := out ++ m!"-equalityLhs: {equalityLhs}\n"
-      let out := out ++ m!"-equalityRhs: {equalityRhs}\n"
-      let out := out ++ m!"-hypsOfEqualityType: {hypsOfType}\n"
+      let out := out ++ m!"-eq.t: {equalityTermType}\n"
+      let out := out ++ m!"-eq.lhs: {equalityLhs}\n"
+      let out := out ++ m!"-eq.rhs: {equalityRhs}\n"
+      let out := out ++ m!"-hypothese of type [eq.t]: {hypsOfEqualityTermType}\n"
+      let out := out ++ m!"-hypotheses of [eq.t = eq.t]: {hypsOfEquality}\n"
       -- let out := out ++ m!"-argumentStx: {argumentStx}\n"
       -- let out := out ++ m!"-mainGoal: {maingoal}\n"
       -- let out := out ++ m!"-goals: {goals}\n"
@@ -45,14 +61,15 @@ elab "myTactic" : tactic =>  do
 -- TODO: Figure out how to extract hypotheses from goal.
 -- | this problem is egg-complete.
 theorem testSuccess : ∀ (anat: Nat) (bint: Int) (cnat: Nat)
-  (dint: Int) (eint: Int) (b_eq_d: bint = dint) (d_eq_e: dint = eint),
+  (dint: Int) (eint: Int) (a_eq_a: anat = anat) (b_eq_d: bint = dint) (d_eq_e: dint = eint),
   bint = eint := by
- intros a b c d e aeqb beqd
+ intros a b c d e aeqa aeqb beqd
  myTactic
  sorry
 
-theorem testInstantiation 
-  (group_inv: forall (g: Int), g - g  = 0) 
+-- | TODO: figure out how to extract out types like the below.
+theorem testInstantiation
+  (group_inv: forall (g: Int), g - g  = 0)
   (h: Int) (k: Int): h - h = k - k := by
  myTactic
  sorry
