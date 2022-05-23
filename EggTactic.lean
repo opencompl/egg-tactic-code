6e1b3ec31cba91f51a4d740ef367c3dc5052f7fb
Siddharth Bhat <siddu.druid@gmail.com>
2022-05-23 23:54:53 +0100

    Actually filter out rewrites perfectly
    Figure out how to run type inference, check that
    the inferred type is as expected. Now we need to
    pass these to egg.

--- EggTactic.lean
+++ EggTactic.lean
@@ -11,6 +11,7 @@ open Lean.Elab.Term
 -- Path to the egg server.
 def eggServerPath : String := "/home/bollu/work/egg/egg-tactic-code/json-egg/target/debug/egg-herbie"
 
+#check inferType
 
 #check Lean.Elab.Tactic.elabTerm
 
@@ -24,9 +25,20 @@ elab "myTactic" "[" rewrites:term,* "]" : tactic =>  withMainContext  do
     -- | elaborate the given hypotheses as equalities.
     -- These are the rewrites we will perform rewrites with.
     let hypsGiven : List Expr <- rewrites.getElems.foldlM (init := []) (fun accum rw_stx => do
-      let tm <- Lean.Elab.Tactic.elabTermEnsuringType rw_stx (Option.some target)
-      let tm <- Term.elabTerm rw_stx (Option.some target)
-      return (tm :: accum)
+      let rw <- Lean.Elab.Tactic.elabTerm rw_stx (Option.none)
+      let rw_type <- inferType rw
+      match rw_type.eq? with
+      | none => throwError "Rewrite |{rw_stx} (term={rw})| must be an equality. Found |{rw} : {rw_type}| which is not an equality"
+      | some (rw_eq_type, rw_lhs, rw_rhs) => do
+         -- | check that rewrite is of the correct type.
+         let is_well_typed <- isExprDefEq rw_eq_type equalityTermType
+         if is_well_typed
+         then return (rw :: accum)
+         else throwError (f!"Rewrite |{rw_stx} (term={rw})| incorrectly equates terms of type |{rw_eq_type}|." ++
+         f!" Expected to equate terms of type |{equalityTermType}|")
+      -- let tm <- Lean.Elab.Tactic.elabTermEnsuringType rw_stx (Option.some target)
+      -- let tm <- Term.elabTerm rw_stx (Option.some target)
+      -- return (tm :: accum)
     )
 
 
@@ -72,15 +84,19 @@ elab "myTactic" "[" rewrites:term,* "]" : tactic =>  withMainContext  do
 
 -- TODO: Figure out how to extract hypotheses from goal.
 -- | this problem is egg-complete.
-def foo : Int := 42
-def bar : 42 = 42 := by { rfl }
+def not_rewrite : Int := 42
+def rewrite_wrong_type : (42 : Nat) = 42 := by { rfl }
+def rewrite_correct_type : (42 : Int) = 42 := by { rfl }
+
+#reduce bar
 
 theorem testSuccess : ∀ (anat: Nat) (bint: Int) (cnat: Nat)
   (dint: Int) (eint: Int) (a_eq_a: anat = anat) (b_eq_d: bint = dint) (d_eq_e: dint = eint),
   bint = eint := by
  intros a b c d e aeqa aeqb beqd
- -- myTactic [bar]
- myTactic [bar]
+--  myTactic [not_rewrite]
+--  myTactic [rewrite_wrong_type]
+ myTactic [rewrite_correct_type]
  sorry
 
 -- | TODO: figure out how to extract out types like the below.
