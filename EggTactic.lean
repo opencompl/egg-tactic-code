aba9472d6f97998a582c81a20faf67f7d02e3adf
Siddharth Bhat <siddu.druid@gmail.com>
2022-05-24 00:24:42 +0100

    Figure out some FVar business
    This now lets us assign cute names to our rewrite LHS and RHSes.

--- EggTactic.lean
+++ EggTactic.lean
@@ -15,6 +15,22 @@ def eggServerPath : String := "/home/bollu/work/egg/egg-tactic-code/json-egg/tar
 
 #check Lean.Elab.Tactic.elabTerm
 
+structure EggRewrite where
+  name: String
+  lhs: String
+  rhs: String
+
+def surroundQuotes (s: String): String :=
+ "\"" ++ s ++ "\""
+
+instance : ToString EggRewrite where
+  toString rewr :=
+    "{"
+      ++ surroundQuotes "name" ++ " = " ++ surroundQuotes rewr.name ++ ","
+      ++ surroundQuotes "lhs" ++ " = " ++ surroundQuotes rewr.lhs ++ ","
+      ++ surroundQuotes "rhs" ++ " = " ++ surroundQuotes rewr.rhs ++
+    "}"
+
 elab "myTactic" "[" rewrites:term,* "]" : tactic =>  withMainContext  do
   let goals <- getGoals
   let target <- getMainTarget
@@ -24,8 +40,8 @@ elab "myTactic" "[" rewrites:term,* "]" : tactic =>  withMainContext  do
     let maingoal <- getMainGoal
     -- | elaborate the given hypotheses as equalities.
     -- These are the rewrites we will perform rewrites with.
-    let hypsGiven : List Expr <- rewrites.getElems.foldlM (init := []) (fun accum rw_stx => do
-      let rw <- Lean.Elab.Tactic.elabTerm rw_stx (Option.none)
+    let hypsGiven : List EggRewrite <- rewrites.getElems.foldlM (init := []) (fun accum rw_stx => withMainContext do
+      let rw <-  (Lean.Elab.Tactic.elabTerm rw_stx (Option.none))
       let rw_type <- inferType rw
       match rw_type.eq? with
       | none => throwError "Rewrite |{rw_stx} (term={rw})| must be an equality. Found |{rw} : {rw_type}| which is not an equality"
@@ -33,7 +49,17 @@ elab "myTactic" "[" rewrites:term,* "]" : tactic =>  withMainContext  do
          -- | check that rewrite is of the correct type.
          let is_well_typed <- isExprDefEq rw_eq_type equalityTermType
          if is_well_typed
-         then return (rw :: accum)
+         then
+           let rw_lhs : Expr <- whnf rw_lhs
+           let lctx <- getLCtx
+           let rw_lhs_decl := LocalContext.findFVar? lctx rw_lhs
+           let exprToString := fun (e: Expr) =>
+              if e.isFVar then toString (lctx.getFVar! e).userName else toString e
+           let lhs_name := exprToString rw_lhs
+           let rhs_name := exprToString rw_rhs
+           dbg_trace "rw_stx: {rw_stx} | rw: {rw} | rw_eq_type: {rw_eq_type} | lhs: {lhs_name} | rhs: {rw_rhs}"
+           let egg_rewrite := { name := toString rw_stx, lhs := toString lhs_name, rhs := toString rhs_name  }
+           return (egg_rewrite :: accum)
          else throwError (f!"Rewrite |{rw_stx} (term={rw})| incorrectly equates terms of type |{rw_eq_type}|." ++
          f!" Expected to equate terms of type |{equalityTermType}|")
       -- let tm <- Lean.Elab.Tactic.elabTermEnsuringType rw_stx (Option.some target)
@@ -88,15 +114,14 @@ def not_rewrite : Int := 42
 def rewrite_wrong_type : (42 : Nat) = 42 := by { rfl }
 def rewrite_correct_type : (42 : Int) = 42 := by { rfl }
 
-#reduce bar
 
 theorem testSuccess : ∀ (anat: Nat) (bint: Int) (cnat: Nat)
   (dint: Int) (eint: Int) (a_eq_a: anat = anat) (b_eq_d: bint = dint) (d_eq_e: dint = eint),
   bint = eint := by
- intros a b c d e aeqa aeqb beqd
+ intros a b c d e aeqa beqd deqe
 --  myTactic [not_rewrite]
 --  myTactic [rewrite_wrong_type]
- myTactic [rewrite_correct_type]
+ myTactic [beqd, deqe]
  sorry
 
 -- | TODO: figure out how to extract out types like the below.
