a76cd093f7504e02c5682af7a33ffc3df485efcb
Siddharth Bhat <siddu.druid@gmail.com>
2022-08-24 12:55:53 +0100

    refactor Sexp: cleaner code, less bugs

--- EggTactic/Sexp.lean
+++ EggTactic/Sexp.lean
@@ -6,7 +6,14 @@ inductive Sexp
 | list: Substring → List Sexp → Sexp
 deriving BEq, Inhabited, Repr
 
-def Sexp.toString : Sexp → String := λ _ => "" -- TODO: implement
+def Sexp.toList! : Sexp → List Sexp
+| .atom ss => panic! s!"found atom at .toList {ss}"
+| .list _ xs => xs
+
+partial def Sexp.toString : Sexp → String
+| .atom ss => ss.toString
+| .list _ xs => "(" ++ " ".intercalate (xs.map Sexp.toString) ++ ")"
+
 instance : ToString Sexp := ⟨Sexp.toString⟩
 
 inductive SexpTok
@@ -15,43 +22,36 @@ inductive SexpTok
 deriving BEq, Inhabited, Repr
 
 
-structure SexpState where 
+structure SexpState where
   it: String.Iterator
   stack: List SexpTok := []
+  sexps: List Sexp := []
+  depth : Nat := 0
 deriving BEq, Repr
 
 def SexpState.fromString (s: String): SexpState :=
-  { it := s.iter, stack := [] : SexpState }
+  { it := s.iter : SexpState }
 
--- abbrev SexpTokStack := List SexpTok
 
--- Remove elements from the stack of tokens `List SexpToken` till we find a `SexpToken.opening`.
--- When we do, return (1) the position of the open paren, (2) the list of SexpTokens left on the stack, and (3) the list of Sexps
--- Until then, accumulate the `SexpToken.sexp`s into `sexps`.
-def stackPopTillOpen (toks: List SexpTok) (sexps: List Sexp := []): Option (String.Pos × (List SexpTok)) × (List Sexp) := 
-  match toks with 
-  | [] => (.none, sexps)
-  | SexpTok.opening openPos :: rest => (.some (openPos, rest), sexps)
-  | SexpTok.sexp s :: rest => stackPopTillOpen rest (s :: sexps)
-  
-
-instance : Inhabited SexpState where 
+instance : Inhabited SexpState where
   default := SexpState.fromString ""
 
 
 inductive SexpError
 | unmatchedOpenParen (ix: String.Iterator): SexpError
 | unmatchedCloseParen (ix: String.Iterator): SexpError
+| notSingleSexp (s: String) (xs: List Sexp): SexpError
 deriving BEq, Repr
 
 instance : ToString SexpError where toString := λ err => match err with
   | .unmatchedOpenParen ix => s!"Unmatched open parenthesis at {ix}"
   | .unmatchedCloseParen ix => s!"Unmatched close parenthesis at {ix}"
+  | .notSingleSexp s xs => s!"not a single sexp '{s}', parsed as: '{xs}'"
 
-abbrev SexpM := EStateM SexpError SexpState 
+abbrev SexpM := EStateM SexpError SexpState
 
 
-def SexpM.peek: SexpM (Option (Char × String.Pos)) := do 
+def SexpM.peek: SexpM (Option (Char × String.Pos)) := do
   let state ← get
   return if state.it.atEnd then .none else .some (state.it.curr, state.it.i)
 
@@ -65,81 +65,118 @@ def SexpM.mkSubstring (l: String.Pos) (r: String.Pos): SexpM Substring := do
   return { str := state.it.s, startPos := l, stopPos := r}
 
 
-def SexpM.advance: SexpM Unit := do 
+def SexpM.advance: SexpM Unit := do
   modify (fun state => { state with it := state.it.next })
 
-def SexpM.push (tok: SexpTok): SexpM Unit := do 
+def SexpM.pushTok (tok: SexpTok): SexpM Unit := do
   modify (fun state => { state with stack := tok :: state.stack })
 
+def SexpM.pushSexp (sexp: Sexp): SexpM Unit := do
+  let state ← get
+  if state.stack.length == 0
+  then set { state with stack := [], sexps := sexp :: state.sexps }
+  else set { state with stack := (SexpTok.sexp sexp) :: state.stack }
 
-instance [Inhabited α] : Inhabited (SexpM α) where 
+
+def SexpM.incrementDepth: SexpM Unit :=
+  modify (fun state => { state with depth := state.depth + 1 })
+
+def SexpM.decrementDepth: SexpM Unit :=
+  modify (fun state => { state with depth := state.depth - 1 })
+
+
+instance [Inhabited α] : Inhabited (SexpM α) where
   default := do return default
 
 
-def SexpM.pop: SexpM SexpTok := do 
+def SexpM.pop: SexpM SexpTok := do
   let state ← get
-  match state.stack with 
+  match state.stack with
   | [] => panic! "empty stack"
-  | x::xs => do 
+  | x::xs => do
       set { state with stack := xs }
       return x
 
+-- abbrev SexpTokStack := List SexpTok
+
+-- Remove elements from the stack of tokens `List SexpToken` till we find a `SexpToken.opening`.
+-- When we do, return (1) the position of the open paren, (2) the list of SexpTokens left on the stack, and (3) the list of Sexps
+-- Until then, accumulate the `SexpToken.sexp`s into `sexps`.
+def stackPopTillOpen (stk: List SexpTok) (sexps: List Sexp := []): Option (String.Pos × (List SexpTok) × (List Sexp)) :=
+  match stk with
+  | [] => .none
+  | SexpTok.opening openPos :: rest => (.some (openPos, rest, sexps))
+  | SexpTok.sexp s :: rest => stackPopTillOpen rest (s :: sexps)
+
+
+
 
 -- collapse the current stack till the last ( into a single Sexp.list
-def SexpM.matchClosingParen: SexpM (List Sexp) := do
+def SexpM.matchClosingParen: SexpM Unit := do
   let state ← get
-  match stackPopTillOpen state.stack with 
-  | (.some (openPos, toks), sexps) => 
+  match stackPopTillOpen state.stack with
+  | (.some (openPos, stk, sexps)) =>
     let substr := Substring.mk state.it.s openPos state.it.i
-    set { state with stack := SexpTok.sexp (Sexp.list substr sexps) :: toks }
-    return sexps
-  | (.none, _) => throw (SexpError.unmatchedCloseParen state.it)
+    let sexp := Sexp.list substr sexps
+    modify (fun state => { state with stack := stk })
+    SexpM.pushSexp sexp
+  | (.none) => throw (SexpError.unmatchedCloseParen state.it)
 
 
-partial def SexpM.takeString (startPos: String.Pos): SexpM Substring := do 
-  match (← SexpM.peek) with 
+partial def SexpM.takeString (startPos: String.Pos): SexpM Substring := do
+  match (← SexpM.peek) with
   | .none => SexpM.mkSubstring startPos (← SexpM.curPos)
   | .some (' ', _) => SexpM.mkSubstring startPos (← SexpM.curPos)
   | .some ('(', _) => SexpM.mkSubstring startPos (← SexpM.curPos)
   | .some (')', _) => SexpM.mkSubstring startPos (← SexpM.curPos)
-  | .some _ => do 
-     SexpM.advance 
+  | .some _ => do
+     SexpM.advance
      SexpM.takeString startPos
 
-partial def SexpM.parse: SexpM (List Sexp) := do 
-  match (← SexpM.peek) with 
-  | .some  ('(', i) => do 
+partial def SexpM.parse: SexpM Unit := do
+  match (← SexpM.peek) with
+  | .some  ('(', i) => do
      SexpM.advance
-     SexpM.push (SexpTok.opening i)
+     SexpM.pushTok (SexpTok.opening i)
+     SexpM.incrementDepth
      SexpM.parse
-  | .some (')', _) => do 
+  | .some (')', _) => do
      SexpM.advance
-     let cur ← SexpM.matchClosingParen
-     let rest ← SexpM.parse
-     return cur ++ rest
-  | .some (' ', _) => do 
+     SexpM.matchClosingParen
+     SexpM.parse
+     -- return cur ++ rest
+  | .some (' ', _) => do
       SexpM.advance
       SexpM.parse
   | .some (_, i) => do
       let s ← SexpM.takeString i
-      SexpM.push (SexpTok.sexp (Sexp.atom s))
+      SexpM.pushSexp ((Sexp.atom s))
       SexpM.parse
   | .none => do
       let state ← get
       match stackPopTillOpen state.stack with
-      | (.some (openPos, _), _) => 
+      | (.some (openPos, _, _)) =>
           throw <| SexpError.unmatchedOpenParen ({ s := state.it.s, i := openPos : String.Iterator })
-      | (.none, sexps) => return sexps
+      | (.none) => return ()
 
-def parseSexp (s: String):  Except SexpError (List Sexp) :=
+-- | Parse a list of (possibly empty) sexps.
+def parseSexpList (s: String):  Except SexpError (List Sexp) :=
   let initState := SexpState.fromString s
   match EStateM.run SexpM.parse initState with
-  | .ok v _ => .ok v
+  | .ok () state => .ok state.sexps.reverse
   | .error e _ => .error e
 
-#eval parseSexp ""
-#eval parseSexp "(a, b)"
-#eval parseSexp "(a, "
-#eval parseSexp "a)"
-#eval parseSexp "a b c"
-#eval parseSexp "(a b) (c d)"
+-- | Parse a single s-expression, and error if found no sexp or multiple sexps
+def parseSingleSexp (s: String): Except SexpError Sexp := do
+  match (← parseSexpList s) with
+  | [x] => .ok x
+  | xs => .error (.notSingleSexp s xs)
+
+
+#eval parseSexpList ""
+#eval parseSexpList "(a, b)"
+#eval parseSexpList "(a, "
+#eval parseSexpList "a)"
+#eval parseSexpList "a b c"
+#eval parseSexpList "(a b) (c d)"
+#eval parseSingleSexp "(a b)"
