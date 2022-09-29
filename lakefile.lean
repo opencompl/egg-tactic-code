bfe58796e734b710045c71e4a8ded61a27fd4ba2
Andres Goens <andres.goens@ed.ac.uk>
2022-09-29 11:35:26 +0100

    Bump nightly

--- lakefile.lean
+++ lakefile.lean
@@ -2,13 +2,12 @@ import Lake
 open Lake DSL
 
 package «egg-tactic» {
-  supportInterpreter := true,
-  libRoots := #[`EggTactic]
-  libName := "EggTactic"
-  binRoot := `Main
-
-  -- add configuration options here
 
 }
+
+lean_lib EggTactic{
+  roots := #[`EggTactic]
+}
+  -- add configuration options here
 
 --require «aesop» from git  "https://github.com/JLimperg/aesop" @ "3fb480b3d7b1e70e488e479e94875bb94d7c8ade"
