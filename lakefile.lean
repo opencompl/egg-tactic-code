52e01bc983e6c7cd757f3dde21fc7097223b2df7
Andres Goens <andres.goens@ed.ac.uk>
2022-09-29 14:11:46 +0100

    add a lake target to call cargo and build the egg binary

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,27 @@ package «egg-tactic» {
 
 }
 
+def compileCargo (name : String) (manifestFile : FilePath)
+ (cargo : FilePath := "cargo") : LogIO Unit := do
+  logInfo s!"Creating {name}"
+  proc {
+    cmd := cargo.toString
+    args := #["build", "--release", "--manifest-path", manifestFile.toString]
+  }
+
+def buildCargo (targetFile : FilePath) (manifestFile : FilePath)
+(oFileJobs : Array (BuildJob FilePath)) : SchedulerM (BuildJob FilePath) :=
+  let name := targetFile.fileName.getD targetFile.toString
+  buildFileAfterDepArray targetFile oFileJobs fun _ => do
+    compileCargo name manifestFile
+
+@[defaultTarget]
+target «egg-herbie» (pkg : Package) : FilePath := do
+  let buildDir := pkg.dir / "json-egg"
+  let binFile := buildDir / "target" / "release" / "egg-herbie"
+  let manifestFile := buildDir / "Cargo.toml"
+  buildCargo binFile manifestFile #[]
+
 lean_lib EggTactic{
   roots := #[`EggTactic]
 }
